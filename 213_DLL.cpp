b94eb960ecd33a80c2007179440a3ea2eabff3f6
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-05-02 15:49:21 +0700

    menambahkan printah untuk masukan nama  ke data field dari node baru

--- 213_DLL.cpp
+++ 213_DLL.cpp
@@ -16,6 +16,7 @@ struct Node
      Node* newNode = new Node(); //step 1:create a new node
     cout << "\nEnter the roll number of the student: ";
     cin >> newNode-> noMhs; // assign value to the data field of the new node
-   
+   cout << "\nEnter the name of the student: ";
+   cin >> newNode-> name;
  }
 
