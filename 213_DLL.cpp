ead28ef2aeb33d02c392eeea827a2fe42fd36c52
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-05-02 15:56:29 +0700

    add decision making if dgn ketentuan start kosong atau data field

--- 213_DLL.cpp
+++ 213_DLL.cpp
@@ -18,5 +18,10 @@ struct Node
     cin >> newNode-> noMhs; // assign value to the data field of the new node
    cout << "\nEnter the name of the student: ";
    cin >> newNode-> name;
- }
+ 
+ 
+ if (START == NULL || newNode-> noMhs == START ->noMhs){
 
+    
+    }
+ };
