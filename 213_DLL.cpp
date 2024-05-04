0d178e9987aa60ab3b78f04235103fe77752957e
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-05-04 08:30:08 +0700

    add fungsi bool list empty

--- 213_DLL.cpp
+++ 213_DLL.cpp
@@ -119,3 +119,7 @@ struct Node
      delete current;
     cout << "\x1b[32mRecord with the roll number" << rollNo << " deleted\x1b[0m" << endl;
 }
+     bool listEmpty()
+{
+    return (START == NULL);
+}
