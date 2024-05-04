2d4de0cd41b860c0fd63385dba2b56635e37e1ed
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-05-04 08:25:57 +0700

    menambahkan perulangan while dan kondisi if untuk menemukan node yang ingin dihapus

--- 213_DLL.cpp
+++ 213_DLL.cpp
@@ -90,4 +90,14 @@ struct Node
     }
      current = START;
     previous = NULL;
+     while(current != NULL && current -> noMhs != rollNo)
+    {
+        previous = current;
+        current = current -> next;
+    }
+    if(current == NULL)
+    {
+        cout << "\033[31mThe e=record with thw roll number"<< rollNo << "not found\033[0m" << endl;
+        return;
+    }
 }
